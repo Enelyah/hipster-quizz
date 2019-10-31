a2dc6518378855587e80f9eb5f96cc75dafce18f
Enelyah <ms.jen.christine@gmail.com>
2019-10-31 21:41:01 +0100

    31-10-2019

---
+++ data.js
@@ -0,0 +1,7 @@
+var stackOfCards =[
+    { name:"test1", isHip:true, source: "./images/test-image.jpg" },
+    { name:"test2", isHip:true, source: "./images/test-image-HIP.jpg" },
+    { name:"test3", isHip:true, source: "./images/test-image-HIP-2.jpg" },
+    { name:"test4", isHip:false, source: "./images/test-image-NOTHIP.jpg" },
+    { name:"test5", isHip:false, source: "./images/test-image-NOTHIP-2.jpg" }
+]
---
+++ gameplay.js
@@ -0,0 +1,6 @@
+var $leftScreen = document.querySelector('.left-screen-blank');
+var $rightScreen = document.querySelector('.right-screen-blank');
+var $topCard = document.querySelector('.stackedcards');
+ $leftScreen.onclick = function (event) {
+  //ex: $topCard.style.display="none"
+ };
