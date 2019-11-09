97dc3bd0fb7c843b138594bee8de0081f900e132
Enelyah <ms.jen.christine@gmail.com>
2019-11-09 12:36:51 +0100

    css frame

--- data.js
+++ data.js
@@ -1,14 +1,16 @@
 const stackOfCards =[
-    { name:"test1", isHip:true, source: "./images/test-image.jpg" },
-    { name:"test2", isHip:true, source: "./images/test-image-HIP.jpg" },
-    { name:"test3", isHip:true, source: "./images/test-image-HIP-2.jpg" },
-    { name:"test4", isHip:false, source: "./images/test-image-NOTHIP.jpg" },
-    { name:"test5", isHip:false, source: "./images/test-image-NOTHIP-2.jpg" },
-    { name:"test6", isHip:true, source: "./images/test-image-1.jpg" },
-    { name:"test7", isHip:true, source: "./images/test-image-HIP-3.jpg" },
-    { name:"test8", isHip:true, source: "./images/test-image-HIP-4.jpg" },
-    { name:"test9", isHip:false, source: "./images/test-image-NOTHIP-3.jpg" },
-    { name:"test10", isHip:false, source: "./images/test-image-NOTHIP-4.jpg" }
+    { name:"hip-01", isHip:true, source: "./images/square-images/hip-01.jpg" },
+    { name:"hip-02", isHip:true, source: "./images/square-images/hip-02.jpg" },
+    { name:"hip-03", isHip:true, source: "./images/square-images/hip-03.jpg" },
+    { name:"hip-04", isHip:false, source: "./images/square-images/hip-04.jpg" },
+    { name:"hip-05", isHip:false, source: "./images/square-images/hip-05.jpg" },
+    { name:"hip-06", isHip:false, source: "./images/square-images/hip-06.jpg" },
+    { name:"nothip-01", isHip:true, source: "./images/square-images/nothip-01.jpg" },
+    { name:"nothip-02", isHip:true, source: "./images/square-images/nothip-02.jpg" },
+    { name:"nothip-03", isHip:true, source: "./images/square-images/nothip-03.jpg" },
+    { name:"nothip-04", isHip:false, source: "./images/square-images/nothip-04.jpg" },
+    { name:"nothip-05", isHip:false, source: "./images/square-images/nothip-05.jpg" },
+    { name:"nothip-06", isHip:true, source: "./images/square-images/nothip-06.jpg" }
 ]
 
 const playedCards=[];
